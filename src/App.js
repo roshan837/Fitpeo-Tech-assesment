4332ebbebc0d70b3737d1c35f2ff3b2e518534f6
Pooja Pandey <poojapandey8890@gmail.com>
2023-10-15 18:43:17 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-function App() {
+function MyComponent() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='main-layout'>
+        <div  className='navigation'>1
+        </div>
+        <div className='main-body'>5
+        </div>
+      </div>
   );
 }
 
-export default App;
+export default MyComponent;
+
